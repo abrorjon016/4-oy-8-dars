86258026d6f215460499d88d97552eab15999625
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-07 02:55:44 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,194 @@
+// 1-masala
+// for (let i = 1; i <= 10; i++) {
+//   let res = i % 2 === 0 ? `${i} juft` : `${i} toq`;
+//   console.log(res);
+// }
+
+// 2-masala
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i > 30 && i < 70) {
+//     let res = i % 2 === 0 ? `${i} juft` : `${i} toq`;
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//     console.log(res);
+//   }
+// }
+// console.log(sum);
+
+// 3-masala
+// let counter = 0;
+// for (let i = -20; i <= 20; i++) {
+//   if (i > 0 && i % 2 !== 0) {
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// 4-masala
+// for (let i = 1; i <= 100; i++) {
+//   let res = i % 2 == 0 || i % 7 == 0 ? `${i} maxsus son` : `${i}`;
+//   console.log(res);
+// }
+
+// Break continue
+
+// 1-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i == 25) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// 2- masala
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 3-masala
+// for (let i = 1; i <= 50; i++) {
+//   if (i > 30) {
+//     break;
+//   }
+//   if (i < 10) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// Switch case
+
+// 1-masala
+// let day = 1;
+
+// switch (day) {
+//   case 1:
+//     console.log(" dushanba");
+//     break;
+//   case 2:
+//     console.log("seshanba");
+//     break;
+//   case 3:
+//     console.log("chorshanba");
+//     break;
+//   case 4:
+//     console.log("pyshanba");
+//     break;
+//   case 5:
+//     console.log("juma");
+//     break;
+//   case 6:
+//     console.log("shanba");
+//     break;
+//   case 7:
+//     console.log("yakshanba");
+//     break;
+//   default:
+//     console.log("bunday kun yoq");
+// }
+
+// 2-masla
+// let month = 1;
+// switch (month) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("yiz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+// }
+
+// 3-masala
+// let num = 1;
+
+// switch (num) {
+//   case 1:
+//     console.log("maktabdan haydaldin");
+//     break;
+//   case 2:
+//     console.log("ota onen bilan kelasan");
+//     break;
+//   case 3:
+//     console.log("endi televizor kormesan");
+//     break;
+//   case 4:
+//     console.log("5 ga harakat qil");
+//     break;
+//   case 5:
+//     console.log("ananib tetee");
+//     break;
+//   default:
+// }
+// bu masalaga biroz ideallik qoshgim keldi )
+
+// 4-masala
+// let num = 1;
+
+// switch (num) {
+//   case 1:
+//     console.log("ekonom");
+//     break;
+//   case 2:
+//     console.log("srtandart");
+//     break;
+//   case 3:
+//     console.log("premium");
+//     break;
+//   default:
+// }
+
+// 5-masala
+// let num = 1;
+// switch (num) {
+//   case 1:
+//     console.log("olma 100 $");
+//     break;
+//   case 2:
+//     console.log("apelsin  200 $");
+//     break;
+//   case 3:
+//     console.log("banan  300 $");
+//     break;
+//   case 4:
+//     console.log("va   400 $");
+//     break;
+//   default:
+// }
+
+// 6-masala
+// let parol = 2;
+
+// switch (parol) {
+//   case 1:
+//     console.log("Parolni tiklash");
+//     break;
+//   case 2:
+//     console.log("Parolni o'zgartirish");
+//     break;
+//   case 3:
+//     console.log("chiqish");
+//     break;
+//   default:
+// }
+ 
+
+// whilega doir masalarga vaqt yetmay qoldi iloji boricha tezroq bajarib push qilishga harakat qlaman ...
